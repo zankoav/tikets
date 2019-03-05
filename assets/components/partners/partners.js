331821d5f97bbbfe8909dd274b3b92d4628f5ab2
Ctrelka <eduard.ignatyuk.1998@mail.ru>
2019-03-05 15:45:48 +0300

    create partners swiper

---
+++ assets/components/partners/partners.js
@@ -0,0 +1,25 @@
+import './partners.scss';
+import Swiper from 'swiper';
+
+new Swiper('.partners__container', {
+    loop: true,
+    navigation: {
+        nextEl: '.partners__button-next',
+        prevEl: '.partners__button-prev',
+    },
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+
+        1023: {
+            slidesPerView: 5,
+            spaceBetween: 16
+        },
+        2560: {
+            slidesPerView: 5,
+            spaceBetween: 32
+        }
+    }
+})
