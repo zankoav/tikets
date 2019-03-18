4adad75a76beacb889f8ad1b2eea33f0c64d019b
zankoav <zankoav@mail.ru>
2019-03-18 19:50:24 +0300

    update

--- assets/templates/program.js
+++ assets/templates/program.js
@@ -12,3 +12,8 @@ import './../components/video-promo/video-promo';
 import './../components/video-review/video-review';
 import './../components/seminarforu/seminarforu';
 import './../components/tabs/tabs';
+import './../components/programm/programm';
+import './../components/tariffs/tariffs';
+import './../components/about_spiker/about_spiker';
+import './../components/block-with-label/block-with-label';
+import './../components/block-without-label/block-without-label';
