df16a2609f06fb40c8708a3082db2465b8260660
Маша Юрлова <mariya.yurlova@gmail.com>
2019-03-18 15:49:30 +0300

    Create Tab About Spiker

--- assets/templates/masha.js
+++ assets/templates/masha.js
@@ -7,5 +7,7 @@ import './../mastak-lib/loader/loader';
 import './../components/header/header';
 import './../components/footer/footer';
 import './../components/programm/programm';
+import './../components/about_spiker/about_spiker';
+
 
 
