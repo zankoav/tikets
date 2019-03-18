a2696494d6cfe0214a41f2ae42be0bb4b231c9b2
Androsov <gilgust090891@gmail.com>
2019-03-18 16:51:31 +0200

    maked base

--- core/carbon/optionPage.php
+++ core/carbon/optionPage.php
@@ -6,7 +6,7 @@
 	
 	add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 	function crb_attach_theme_options(){
-		Container::make( 'theme_options', __( 'Настройки темы' ) )
+		$basic_options_container = Container::make( 'theme_options', __( 'Настройки темы' ) )
 			->set_page_file( 'theme-options' )
 			->set_page_menu_title( 'Настройки темы' )
 			->set_icon( 'dashicons-carrot' )
@@ -29,9 +29,42 @@
 				
 				//Field::make( 'oembed', 'crb_oembed', __( 'oEmbed' ) )
 			] );
+		
+		// Add second options page under 'Basic Options'
+		Container::make( 'theme_options', 'Настройки WEBPAY' )
+			->set_page_parent( $basic_options_container ) // reference to a top level container
+			->add_fields( array(
+				
+				Field::make( 'text', 'wsb_store','wsb_store' )
+					->set_required( true ),
+				Field::make( 'text', 'wsb_secret_key','secret key' )
+					->set_required( true ),
+				Field::make( 'text', 'wsb_storeid','wsb_storeid' )
+					->set_required( true ),
+				Field::make( 'select', 'wsb_return_url', 'Выберите страницу для удачного заказа' )
+					->add_options( 'page_selecting' )
+					->set_required( true ),
+				Field::make( 'select', 'wsb_cancel_return_url','Выберите страницу для неудачного заказа' )
+					->add_options( 'page_selecting' )
+					->set_required( true ),
+				Field::make( 'select', 'wsb_notify_url','Выберите страницу для извещения о оплате' )
+					->add_options( 'page_selecting' )
+					->set_required( true ),
+			) );
 	}
 	
 	add_action( 'after_setup_theme', 'crb_load' );
 	function crb_load(){
 		\Carbon_Fields\Carbon_Fields::boot();
+	}
+	
+	function page_selecting() {
+		$my_query = new WP_Query();
+		$query_news =$my_query->query(['post_type' => 'page']);
+		
+		$news_list =[];
+		foreach( $query_news  as $news ){
+			$news_list[$news->ID] = $news->post_title;
+		}
+		return $news_list ;
 	}
