933dafcd93630d43ee74e61b03a7a43ae3ae793a
Androsov <gilgust090891@gmail.com>
2019-03-19 14:40:30 +0200

    fix home html

--- template-home.php
+++ template-home.php
@@ -11,29 +11,25 @@
 <div class="app">
 	<?php get_template_part( '/core/views/headerView' ); ?>
 	<main class="main">
-
-		<?php get_template_part('/core/views/home','slider')?>
-		<div class="features">
-			<div class="container features_mobile">
-				<div class="features__inner">
-					<?php get_template_part( '/core/views/home','benefits' ); ?>
-					<div class="features__pagination-wrapper pos-a">
-						<div class="features__pagination swiper-pagination">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php get_template_part('/core/views/home','stock')?>
-		<?php get_template_part('/core/views/home','seminarforu')?>
-		<?php get_template_part('/core/views/home','reviews')?>
-		<?php get_template_part('/core/views/home','news')?>
+		
+		<?php get_template_part( '/core/views/home', 'slider' ) ?>
+		<?php get_template_part( '/core/views/home', 'benefits' ); ?>
+		<?php get_template_part( '/core/views/home', 'stock' ) ?>
+		<?php get_template_part( '/core/views/home', 'seminarforu' ) ?>
+		<?php get_template_part( '/core/views/home', 'reviews' ) ?>
+		<?php get_template_part( '/core/views/home', 'news' ) ?>
 		<div class="mb-40">
 			<div class="partnership">
 				<div class="container">
 					<div class="partnership__inner">
 						<h2 class="title title_grey">ПАРТНЕРСТВО</h2>
-						<div class="mt-20 mb-20 mt-sm-40 mb-sm-40 pt-md-15"><a class="partnership__download pb-05 pb-sm-10" href="http://tut.by/sdvbsjbdv.pdf">скачайте коммерческое предложение для партнеров скачайте коммерческое предложение для партнеров</a><a class="partnership__download" href="http://tut.by/sdvbsjbdv.pdf">скачайте коммерческое предложение для партнеров</a>
+						<div class="mt-20 mb-20 mt-sm-40 mb-sm-40 pt-md-15">
+							<a class="partnership__download pb-05 pb-sm-10" href="http://tut.by/sdvbsjbdv.pdf">скачайте
+								коммерческое предложение для партнеров скачайте коммерческое предложение для партнеров
+							</a>
+							<a class="partnership__download" href="http://tut.by/sdvbsjbdv.pdf">скачайте коммерческое
+								предложение для партнеров
+							</a>
 						</div>
 					</div>
 					<form id="partnership__form" method="POST" action="/">
@@ -56,7 +52,9 @@
 							</div>
 							<div class="col-12 col-sm-6 col-offset-sm-2 pt-sm-20 pt-md-40">
 								<div class="partnership__button">
-									<button class="button button__color_red" type="submit" name="userbutton">Записаться</button>
+									<button class="button button__color_red" type="submit" name="userbutton">
+										Записаться
+									</button>
 								</div>
 							</div>
 						</div>
