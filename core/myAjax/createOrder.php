b56a930dfafae357fa351caffdcc80985e9011d1
Androsov <gilgust090891@gmail.com>
2019-03-21 14:57:02 +0200

    add ajax method

--- core/myAjax/createOrder.php
+++ core/myAjax/createOrder.php
@@ -1,51 +1,74 @@
 <?php
 	
-	if (!defined('ABSPATH')) {exit();}
-
-//add_filter( 'wp_mail_content_type', 'set_html_content_type' );
-	
-	function contact_form()
-	{
-	
-//		$options = SingletonOptions::getOptions();
-//		$mail_to = $options['email'];
-//		$subject = __('Zankoav question!', THEME_NAME);
-//		$headers = 'From: Zankoav Web Site <zankoav@mail.ru>' . "\r\n";
-//
-//		$response = array();
-//		$response['status'] = 0;
-//
-//		$form_name = empty($_POST['name']) ? '' : esc_attr($_POST['name']);
-//		$form_email = empty($_POST['email']) ? '' : esc_attr($_POST['email']);
-//		$form_message = empty($_POST['message']) ? '' : esc_attr($_POST['message']);
-//		$form_spam = empty($_POST['spam']) ? '' : esc_attr($_POST['spam']);
-//
-//		if (!empty($form_spam)) {
-//			return;
-//		}
-//
-//		if (!filter_var($form_email, FILTER_VALIDATE_EMAIL)) {
-//
-//			$response['status'] = 2;
-//			echo json_encode($response);
-//			wp_die();
-//		}
-//
-//
-//		$msg = "<p><strong>Name: </strong><span>" . $form_name . "</span></p>
-//			<p><strong>Email: </strong><span>" . $form_email . "</span></p>
-//			<p><strong>Message: </strong><span>" . $form_message . "</span></p>";
-//
-//
-//		if (wp_mail($mail_to, $subject, $msg, $headers)) {
-//			$response['status'] = 1;
-//		}
-//
-//		echo json_encode($response);
-//		wp_die();
+	if (!defined( 'ABSPATH' )) {
+		exit();
 	}
 	
-	//	add_action( 'wp_ajax_contact_form', 'contact_form' );
-	//	add_action( 'wp_ajax_nopriv_contact_form', 'contact_form' );
-
-
+	add_action( 'wp_ajax_createOrder', 'createOrder' );
+	add_action( 'wp_ajax_nopriv_createOrder', 'createOrder' );
+	function createOrder(){
+		$form_data = [
+			'customer_address' => empty( $_POST[ 'customer_address' ] ) ? '' : esc_attr( $_POST[ 'customer_address' ] ),
+			'customer_name'    => empty( $_POST[ 'customer_name' ] ) ? '' : esc_attr( $_POST[ 'customer_name' ] ),
+			'item_name'        => empty( $_POST[ 'item_name' ] ) ? '' : esc_attr( $_POST[ 'item_name' ] ),
+			'item_price'       => empty( $_POST[ 'item_price' ] ) ? '' : esc_attr( $_POST[ 'item_price' ] ),
+			'item_quantity'    => empty( $_POST[ 'item_quantity' ] ) ? '' : esc_attr( $_POST[ 'item_quantity' ] ),
+			'product_id'       => empty( $_POST[ 'product_id' ] ) ? '' : esc_attr( $_POST[ 'product_id' ] ),
+			'ticket_type'      => empty( $_POST[ 'ticket_type' ] ) ? '' : esc_attr( $_POST[ 'ticket_type' ] ),
+			'wsb_email'        => empty( $_POST[ 'wsb_email' ] ) ? '' : esc_attr( $_POST[ 'wsb_email' ] ),
+		];
+		
+		if (!filter_var( $form_data[ 'wsb_email' ], FILTER_VALIDATE_EMAIL )) {
+			$response[ 'status' ] = 2;
+			echo json_encode( $response );
+			wp_die();
+		}
+		
+//		createOrderEntity( $form_data );
+//		echo json_encode( $response );
+		wp_die();
+	}
+	
+	
+	
+	function createOrderEntity($form_data){
+		$address = array(
+			'first_name' => $form_data['customer_name'],
+			'last_name'  => '',
+			'company'    => '',
+			'email'      => $form_data['wsb_email'],
+			'phone'      => '',
+			'address_1'  => $form_data['customer_address'],
+			'address_2'  => '',
+			'city'       => '',
+			'state'      => '',
+			'postcode'   => '',
+			'country'    => ''
+		);
+		
+		$order = wc_create_order();
+		
+		// add products from cart to order
+//		$items = WC()->cart->get_cart();
+		foreach($items as $values) {
+			$product_id = $values['product_id'];
+			$product = wc_get_product($product_id);
+			$var_id = $values['variation_id'];
+			$var_slug = $values['variation']['attribute_pa_weight'];
+			$quantity = (int)$values['quantity'];
+			$variationsArray = array();
+			$variationsArray['variation'] = array(
+				'pa_weight' => $var_slug
+			);
+			$var_product = new WC_Product_Variation($var_id);
+			$order->add_product($var_product, $quantity, $variationsArray);
+		}
+		
+		$order->set_address( $address, 'billing' );
+		$order->set_address( $address, 'shipping' );
+		
+		$order->calculate_totals();
+		$order->update_status( 'processing' );
+		
+		WC()->cart->empty_cart();
+	}
--- template-checkout.php
+++ template-checkout.php
@@ -2,7 +2,7 @@
 	/**
 	 * Template Name: Checkout Order Template
 	 */
-	get_header('checkout');
+	get_header( 'checkout' );
 	//	$wsb_secret_key = carbon_get_theme_option('wsb_secret_key');
 	
 	$wsb_store             = carbon_get_theme_option( 'wsb_store' );
@@ -14,6 +14,7 @@
 	$productID  = $_GET[ 'seminar' ];
 	$_product   = wc_get_product( $productID );
 	$prod_exist = empty( $_product ) ? false : true;
+	
 	if ($prod_exist) {
 		$prodName   = $_product->get_title();
 		$variations = $_product->get_available_variations();
@@ -22,17 +23,16 @@
 		foreach($variations as $variation) {
 			$variations_select[] = [
 				'variation' => $variation[ "attributes" ][ "attribute_pa_tip-bileta" ],
-				'$price' => $variation['display_regular_price'],
+				'$price'    => $variation[ 'display_regular_price' ],
 			];
-//			if ($variation[ "attributes" ][ "attribute_pa_tip-bileta" ] == "busines") {
-//				$business_ticket = $variation;
-//			} elseif ($variation[ "attributes" ][ "attribute_pa_tip-bileta" ] == "vip") {
-//				$vip_ticket = $variation;
-//			} elseif ($variation[ "attributes" ][ "attribute_pa_tip-bileta" ] == "platinum") {
-//				$platinum_ticket = $variation;
-//			} elseif ($variation[ "attributes" ][ "attribute_pa_tip-bileta" ] == "standard") {
-//				$standard_ticket = $variation;
-//			}
+		}
+		$test_type = [
+			'variation'  => 'standard',
+			'$price' => 1313
+		];
+		
+		if (!empty($variations[ 0 ])){
+			$test_type = $variations[ 0 ];
 		}
 	}
 
@@ -40,65 +40,73 @@
 <div class="app">
 	<?php get_template_part( '/core/views/headerView' ); ?>
 	<main class="main">
-		<?php if (!$prod_exist): ?>
-			<h1>Семенар не найден</h1>
-		<?php else: ?>
-			<select id="ticket_type" name="ticket_type">
-				<option disabled selected>Выберите тип билета</option>
-				<?php foreach($variations_select as $item) :
-					if ($item['variation'] == 'standard'):?>
-						<option selected value="<?= $item['variation'];?>" data-price="<?= $item['$price']; ?>"><?= $item['variation']; ?></option>
-				<?php else:?>
-				<option value="<?= $item['variation'];?>" data-price="<?= $item['$price']; ?>"><?= $item['variation']; ?></option>
-				<?php endif; endforeach;?>
-			</select>
-			<form action="" method="post" style="
+		<?php
+			
+			if (!$prod_exist): ?>
+				<h1>Семенар не найден</h1>
+			<?php else: ?>
+				<p id="product_id"><?= $productID; ?></p>
+				<select id="ticket_type" name="ticket_type">
+					<?php foreach($variations_select as $item) :
+						if ($item[ 'variation' ] == $test_type[ 'variation' ]):?>
+							<option selected value="<?= $item[ 'variation' ]; ?>"
+									data-price="<?= $item[ '$price' ]; ?>"><?= $item[ 'variation' ]; ?></option>
+						<?php else: ?>
+							<option value="<?= $item[ 'variation' ]; ?>"
+									data-price="<?= $item[ '$price' ]; ?>"><?= $item[ 'variation' ]; ?></option>
+						<?php
+						
+						endif; endforeach; ?>
+				</select>
+				<form action="" method="post" style="
 			  display: flex;
 			  flex-direction: column;
 			  width: 500px;
 			  margin: 1rem auto;
 ">
 
-				<!--			<form action="https://securesandbox.webpay.by/" method="post" style="-->
-				<!--			  display: flex;-->
-				<!--			  flex-direction: column;-->
-				<!--			  width: 500px;-->
-				<!--			  margin: 1rem auto;-->
-				<!--">-->
-				<input type="hidden" name="*scart">
-				<input type="hidden" name="wsb_storeid" value="<?= $wsb_storeid ?>">
-				<input type="hidden" name="wsb_currency_id" value="BYN">
-				<input type="hidden" name="wsb_version" value="2">
-				<input type="hidden" name="wsb_test" value="1">
-				<!--			<input type="hidden" name="wsb_store" value="--><? //= $wsb_store?><!--">-->
+					<!--			<form action="https://securesandbox.webpay.by/" method="post" style="-->
+					<!--			  display: flex;-->
+					<!--			  flex-direction: column;-->
+					<!--			  width: 500px;-->
+					<!--			  margin: 1rem auto;-->
+					<!--">-->
+					<input type="hidden" name="*scart">
+					<input type="hidden" name="wsb_storeid" value="<?= $wsb_storeid ?>">
+					<input type="hidden" name="wsb_currency_id" value="BYN">
+					<input type="hidden" name="wsb_version" value="2">
+					<input type="hidden" name="wsb_test" value="1">
+					<!--			<input type="hidden" name="wsb_store" value="--><? //= $wsb_store?><!--">-->
 
-				<!--create order-->
-				<input type="hidden" name="wsb_order_num" value="ORDER-12345678">
-				<input type="hidden" name="wsb_seed" value="1242649174">
-				<label for="wsb_invoice_item_name[0]">Название товара:</label>
-				<input name="wsb_invoice_item_name[0]" type="text" value="<?= $prodName; ?>">
-				<label for="wsb_invoice_item_price[0]"> Цена:</label>
-				<input name="wsb_invoice_item_price[0]" type="text" value="10">
-				<input type="hidden" name="wsb_invoice_item_quantity[0]" value="1">
+					<label for="wsb_invoice_item_name[0]">Название товара:</label>
+					<input name="wsb_invoice_item_name[0]" type="text" value="<?= $prodName; ?>">
+					<label for="wsb_invoice_item_price[0]"> Цена:</label>
+					<input name="wsb_invoice_item_price[0]" type="text" value="<?= $test_type[ 'display_regular_price' ]; ?>">
+					<input type="hidden" name="wsb_invoice_item_quantity[0]" value="1">
 
-				<input type="hidden" name="wsb_signature" value="912702512e447846add6fa4985c7a2f271de52e6">
 
-				<label for="wsb_customer_name">Имя</label>
-				<input type="text" name="wsb_customer_name" value="">
-				<label for="wsb_customer_address">Адрес</label>
-				<input type="text" name="wsb_customer_address" value="">
-				<label for="wsb_email">email</label>
-				<input type="text" name="wsb_email" value="email">
+					<label for="wsb_customer_name">Имя</label>
+					<input type="text" name="wsb_customer_name" value="sas">
+					<label for="wsb_customer_address">Адрес</label>
+					<input type="text" name="wsb_customer_address" value="ул. короля д.55">
+					<label for="wsb_email">email</label>
+					<input type="text" name="wsb_email" value="sas@sas.sas">
 
-				<input type="hidden" name="wsb_return_url" value="<?= esc_url( $wsb_return_url ); ?>">
-				<input type="hidden" name="wsb_cancel_return_url" value="<?= esc_url( $wsb_cancel_return_url ); ?>">
-				<input type="hidden" name="wsb_notify_url" value="<?= esc_url( $wsb_cancel_return_url ); ?>">
+					<input type="hidden" name="wsb_return_url" value="<?= esc_url( $wsb_return_url ); ?>">
+					<input type="hidden" name="wsb_cancel_return_url" value="<?= esc_url( $wsb_cancel_return_url ); ?>">
+					<input type="hidden" name="wsb_notify_url" value="<?= esc_url( $wsb_cancel_return_url ); ?>">
 
-				<input type="hidden" name="wsb_total" value="10.00">
 
-				<input type="submit" id="submit-button" value="Перейти к оплате">
-			</form>
-		<?php endif; ?>
+					<!--return from back end-->
+					<input type="hidden" name="wsb_total" value="10.00">
+					<input type="hidden" name="wsb_signature" value="912702512e447846add6fa4985c7a2f271de52e6">
+					<!--create order-->
+					<input type="hidden" name="wsb_order_num" value="ORDER-12345678">
+					<input type="hidden" name="wsb_seed" value="1242649174">
+
+					<input type="submit" id="submit-button" value="Перейти к оплате">
+				</form>
+			<?php endif; ?>
 	</main>
 	<?php get_template_part( '/core/views/footerView' ); ?>
 </div>
